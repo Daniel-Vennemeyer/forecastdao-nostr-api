16cef516e6ed6e0abf09e4d322b0d6a5542b2af6
Daniel Vennemeyer <77549269+Daniel-Vennemeyer@users.noreply.github.com>
2023-02-22 15:06:21 -0500

    Clarify run port

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ def hello_world():
 	return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run()  # run our Flask app
-    # app.run(host='127.0.0.1', port=3000, debug=True)
+    # app.run()  # run our Flask app
+    app.run(host='127.0.0.1', port=5000)
