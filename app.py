58f763ced1f73303e2427ed011bde03db8cd8f1d
Daniel Vennemeyer <77549269+Daniel-Vennemeyer@users.noreply.github.com>
2023-03-29 18:46:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def clean(): #Get just the values for the data from the nostr relay
     try:
         data = Data.Data().get()
         # data = requests.get(url="http://3.144.27.94:5000/data").text
-        data = data
+        # data = data.replace("\n", "")
         data = json.loads(data)
         cleaned = []
         for event in data:
